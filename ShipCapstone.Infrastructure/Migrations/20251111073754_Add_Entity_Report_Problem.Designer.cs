c61fed631e2d4d2a62fc7959793226578ca9787f
longsmile2k <longsmile2k@gmail.com>
2025-11-12 13:03:00 +0700

    migration entity

--- ShipCapstone.Infrastructure/Migrations/20251111073754_Add_Entity_Report_Problem.Designer.cs
+++ ShipCapstone.Infrastructure/Migrations/20251111073754_Add_Entity_Report_Problem.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShipCapstone.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using ShipCapstone.Infrastructure.Persistence;
 namespace ShipCapstone.Infrastructure.Migrations
 {
     [DbContext(typeof(ShipCapstoneContext))]
-    partial class ShipCapstoneContextModelSnapshot : ModelSnapshot
+    [Migration("20251111073754_Add_Entity_Report_Problem")]
+    partial class Add_Entity_Report_Problem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ShipCapstone.Infrastructure/Migrations/20251111073754_Add_Entity_Report_Problem.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShipCapstone.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Entity_Report_Problem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Order",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "ReportProblem",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PortId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ShipId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportProblem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReportProblem_Port_PortId",
+                        column: x => x.PortId,
+                        principalTable: "Port",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ReportProblem_Ship_ShipId",
+                        column: x => x.ShipId,
+                        principalTable: "Ship",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReportProblem_PortId",
+                table: "ReportProblem",
+                column: "PortId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReportProblem_ShipId",
+                table: "ReportProblem",
+                column: "ShipId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReportProblem");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Order");
+        }
+    }
+}
