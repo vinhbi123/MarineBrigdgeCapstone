ddb03522039580812d3a267331d4b0cec5be3f31
longsmile2k <longsmile2k@gmail.com>
2025-11-16 23:06:38 +0700

    product controller

--- ShipCapstone.Application/Controllers/ProductsController.cs
+++ ShipCapstone.Application/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ShipCapstone.Application.Common.Utils;
 using ShipCapstone.Application.Common.Validators;
+using ShipCapstone.Application.Features.Products.Command.CreateProduct;
 using ShipCapstone.Application.Features.Products.Command.UpdateProduct;
 using ShipCapstone.Application.Features.Products.Query.GetProductById;
 using ShipCapstone.Domain.Models.Common;
@@ -23,3 +24,30 @@ public class ProductsController : BaseController<ProductsController>
         var apiResponse = await _mediator.Send(query);
         return Ok(apiResponse);
     }
+    /// <summary>
+    /// Create new product
+    /// POST /api/v1/products
+    /// </summary>
+    [HttpPost]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> CreateProduct([FromBody] CreateProductCommand command,
+        [FromServices] ValidationUtil<CreateProductCommand> validationUtil)
+    {
+        var (isValid, response) = await validationUtil.ValidateAsync(command);
+        if (!isValid) return BadRequest(response);
+
+        var apiResponse = await _mediator.Send(command);
+
+        // trả Location tới GET product by id nếu apiResponse.Data = createdId
+        if (apiResponse?.Data is Guid createdId)
+        {
+            return CreatedAtAction(nameof(GetProductById), new { id = createdId }, apiResponse);
+        }
+
+        // fallback
+        return StatusCode(StatusCodes.Status201Created, apiResponse);
+    }
+}
