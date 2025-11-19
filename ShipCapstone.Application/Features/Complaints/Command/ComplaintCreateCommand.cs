25d12e2355f4a6047b4570a7dbeb43a3217d50a8
longsmile2k <longsmile2k@gmail.com>
2025-11-19 23:37:34 +0700

    create complaint

---
+++ ShipCapstone.Application/Features/Complaints/Command/ComplaintCreateCommand.cs
@@ -0,0 +1,12 @@
+﻿using Mediator;
+using ShipCapstone.Domain.Models.Common;
+
+namespace ShipCapstone.Application.Features.Complaints.Command.CreateComplaint;
+
+public class CreateComplaintCommand : IRequest<ApiResponse>
+{
+    public string Content { get; set; }
+    public Guid? OrderId { get; set; }
+    public Guid? BookingId { get; set; }
+    public Guid ReceiverAccountId { get; set; } 
+}
---
+++ ShipCapstone.Application/Features/Complaints/Command/ComplaintCreateValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace ShipCapstone.Application.Features.Complaints.Command.CreateComplaint;
+
+public class CreateComplaintCommandValidator : AbstractValidator<CreateComplaintCommand>
+{
+    public CreateComplaintCommandValidator()
+    {
+        RuleFor(x => x.Content)
+            .NotEmpty().WithMessage("Nội dung khiếu nại không được để trống");
+
+        RuleFor(x => x)
+            .Must(x => x.OrderId != null || x.BookingId != null)
+            .WithMessage("Complaint phải liên quan đến Order hoặc Booking");
+
+        RuleFor(x => x.ReceiverAccountId)
+            .NotEmpty().WithMessage("ReceiverAccountId không được để trống");
+    }
+}
